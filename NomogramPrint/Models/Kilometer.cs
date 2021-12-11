e12490b6dea5a0bebb020f883f1f02dbd56b5f02
aVor42 <an_vor27@mail.ru>
2021-12-11 18:41:22 +1000

    first commit

---
+++ NomogramPrint/Models/Kilometer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace NomogramPrint.Models
+{
+    public class Kilometer
+    {
+
+        public Kilometer() :
+            this(0)
+        { }
+
+        public Kilometer(int number)
+        {
+            Number = number;
+            Objects = new List<IObject>();
+        }
+
+        public int Number { get; set; }
+        public List<IObject> Objects { get; set; }
+    }
+}
---
+++ NomogramPrint/Servicies/ExcelParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NomogramPrint.Models;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+
+
+namespace NomogramPrint.Servicies
+{
+    public class ExcelParser
+    {
+        public DbContext GetData()
+        {
+            (int, int) km = getKm();
+            DbContext context = new DbContext();
+            for (int i = km.Item2; i > km.Item1; i--)
+                context.Kilometers.Add(new Kilometer { Number = i});
+            context.Kilometers[18].Objects.Add(new KTSM());
+            return context;
+        }
+        public Range readFile(string path)
+        {
+            //Create COM Objects.
+            Application excelApp = new Application();
+            Workbook excelBook = excelApp.Workbooks.Open(path);
+            _Worksheet excelSheet = excelBook.Sheets[1];
+            Range excelRange = excelSheet.UsedRange;
+            //excelApp.Quit();
+            //System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            return excelRange;
+        }
+        public (int, int) getKm()
+        {
+            Range excelRange = readFile(@"D:\hak\Killometrazh.xlsx");
+            int rows = excelRange.Rows.Count;
+            int cols = excelRange.Columns.Count;
+            var startKm = (int)excelRange.Cells[1, 1].Value2;
+            var endKm = (int)excelRange.Cells[1, 2].Value2;
+            //after reading, relaase the excel project
+            return (startKm, endKm);
+        }
+    }
+}
